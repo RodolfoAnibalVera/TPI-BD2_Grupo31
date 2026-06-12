eaf92c2c9d1b84bb6ee0a611493ef1e22c89e988
RodolfoAnibalVera <vrodolfoanibal@gmail.com>
2026-06-11 22:03:14 -0300

    Modifico metodo Eliminar en Negocio/PedidoNegocio.cs  para capturar mensaje de error lanzado por el trigger TR_SoloEliminarPedidosCancelados

--- Sistema/Negocio/PedidoNegocio.cs
+++ Sistema/Negocio/PedidoNegocio.cs
@@ -1,6 +1,7 @@
 ﻿using Dominio;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -410,7 +411,14 @@ namespace Negocio
                 datos.setearParametro("@id", id);
                 datos.ejecutarAccion();
             }
-            finally { datos.cerrarConexion(); }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
         }
 
         public Pedido ObtenerPedido(int id)
